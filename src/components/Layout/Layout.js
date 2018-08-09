3c505830a76da86f1f64136ce6202fbbaa978393
Pham <pham.paul.q@gmail.com>
2018-08-09 15:40:59 -0500

    added lecturer's implementation of drawer toggle

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,16 +7,21 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
     sideDrawerClosedhandler = () => {
         this.setState({showSideDrawer: false})
+    }
+    sideDrawerTogglehandler = () => {
+        this.setState((prevState) => {
+           return {showSideDrawer: !prevState.showSideDrawer};
+        });
     }
 
     render () {
         return (
             <Aux>
-                <Toolbar />
+                <Toolbar drawerToggleClicked={this.sideDrawerTogglehandler}/>
                 <SideDrawer 
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedhandler}/>
